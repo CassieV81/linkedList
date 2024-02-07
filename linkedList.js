b31a69026b76b31d03ad29ec58b63bd3151fd9cf
Cassandra <s18c82@umwelt-campus.de>
2024-02-07 18:10:11 +0100

    created linked list project

---
+++ linkedList.js
@@ -0,0 +1,215 @@
+
+
+class Node {
+    constructor(value=null) {
+        this.value = value;
+        this.next = null;
+    }
+}
+let node = new Node();
+console.log(node);
+
+class LinkedList {
+    constructor() {
+        this.head = null;
+    }
+    
+    append(value) {
+        let tail = null;
+        if (this.head == null) {
+            this.head = new Node(value);
+        } else {
+            tail = this.head;
+            while (tail.next != null) {
+                tail = tail.next;
+            }
+            tail.next = new Node(value);
+        }
+    }
+
+    prepend(value) {
+        if (this.head == null) {
+            this.head = new Node(value);
+        } else {
+            let list = this.head;
+            this.head = new Node(value);
+            this.head.next = list;
+        }
+    }
+
+    size() {
+        let counter = 1;
+        let list = this.head;
+        while (list.next != null) {
+            list = list.next;
+            counter += 1;
+        }
+        return counter;
+    }
+
+    listHead() {
+        return this.head;
+    }
+
+    tail() {
+        let list = this.head;
+        while (list) {
+            if (list.next == null) {
+                break;
+            }
+            list = list.next;
+        }
+        return list;
+    }
+
+    at(index) {
+        let list = this.head;
+        if (index >= this.length) return null;
+        if (index === 0) return list;
+        while (list) {
+            if (index === 0) {
+                break;
+            }
+            list = list.next;
+            index -= 1;
+        }
+        return list;
+    }
+
+    pop() {
+        let list = this.head;
+        let popped;
+        while (list) {
+            if (list.next.next == null) {
+                popped = list.next.value;
+                list.next = null;
+                break;
+            }
+            list = list.next;
+        }
+        return popped;
+    }
+
+    contains(value) {
+        let list = this.head;
+        let status = false;
+        if (list.value === value) return true;
+        while (list.next !== null) {
+            if (list.next.value === value) {
+                status = true;
+            } 
+            list = list.next;
+        }
+        return status;
+    }
+
+    find(value) {
+        let index = 1;
+        let list = this.head;
+        if (list.value === value) return 0;
+        while (list.next !== null) {
+            if (list.next.value === value) break;
+            if (list.next.value !== value && list.next.next === null) return null;
+            index += 1;
+            list = list.next;
+        }
+        return index;
+    }
+
+    toString() {
+        let list = this.head;
+        let string = `${list.value}`;
+        while (list.next !== null) {
+            string += ` -> ${list.next.value}`;
+            list = list.next;
+        }
+        return string += ` -> ${null}`;
+    }
+
+    insertAt(value, index) {
+        if (index >= this.size()) return this.append(value);
+        let list = this.head;
+        let tail = this.at(index);
+        let values = [];
+        let tailValues = [];
+        this.head = null;
+        if (index === 0) {
+            this.head = new Node(value);
+            return this.head.next = list;
+        };
+        while(list.next !== null) {
+            if (list.value === tail.value) {
+                break;
+            }
+            values.push(list.value);
+            list = list.next
+        }
+        while(tail) {
+            tailValues.push(tail.value);
+            tail = tail.next
+        }
+        while(values.length) {
+            this.append(values.shift())
+            if (values.length === 0) {
+                this.append(value);
+            }
+        }
+        while(tailValues.length) {
+            this.append(tailValues.shift())
+        }
+    }
+
+    removeAt(index) {
+        if (index >= this.size()) return null;
+        let list = this.head;
+        let tail = this.at(index);
+        let values = [];
+        let tailValues = [];
+        this.head = null;
+        if (index === 0) {
+            let value = list.next.value;
+            this.head = new Node(value);
+            return this.head.next = list.next.next;
+        };
+        while(list.next !== null) {
+            if (list.value === tail.value) {
+                break;
+            }
+            values.push(list.value);
+            list = list.next
+        }
+        while(tail) {
+            tailValues.push(tail.value);
+            tail = tail.next
+        }
+        while(values.length) {
+            this.append(values.shift())
+        }
+        tailValues.shift()
+        while(tailValues.length) {
+            this.append(tailValues.shift())
+        }
+    }
+
+}
+let list = new LinkedList();
+console.log(list);
+list.append(4);
+list.prepend(3);
+list.append(5);
+list.append(1);
+list.append(10);
+list.prepend(50);
+console.log(list.size());
+// console.log(list.listHead());
+// console.log(list.at(1));
+// console.log(list.pop());
+// console.log(list.pop());
+console.log(list.toString());
+// console.log(list.contains(3));
+list.insertAt(7, 20);
+console.log(list.size());
+list.insertAt(0, 5);
+console.log(list.toString());
+list.removeAt(7)
+console.log(list.toString());
